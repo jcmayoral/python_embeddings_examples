79b8dae1cb2aa1c86ebee45e76aeae5a2ac17fba
Jose Mayoral <jocamaba1989@gmail.com>
2023-08-22 15:39:07 -0500

    first commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -I/usr/include/python3.10/ main.cpp  -L/usr/lib/python3.10/config-3.10-x86_64-linux-gnu/ -lpython3.10 -o output
---
+++ main.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+#define PY_SSIZE_T_CLEAN
+
+#include </usr/include/python3.10/Python.h>
+//#include <Python.h>
+#include<iostream>
+
+int main(int argc, char** argv){
+    std::cout << "hello" <<  std::endl;
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+
+    if (program == NULL){
+        fprintf(stderr, "Fatal Error: Program cannot decode");
+        exit(1);
+    }
+
+    Py_SetProgramName(program);
+    Py_Initialize();
+    PyRun_SimpleString("print('hello world python')");
+
+    if (Py_FinalizeEx() < 0){
+        exit(120);
+    }
+
+    PyMem_RawFree(program);
+    return 0;
+}
